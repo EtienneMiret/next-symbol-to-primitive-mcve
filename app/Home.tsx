c11927f30b50d246647135e1990569c64d953f01
Étienne Miret <etienne.miret@ens-lyon.org>
2023-06-08 17:20:02 +0200

    Create homepage

---
+++ app/Home.tsx
@@ -0,0 +1,8 @@
+export const title = 'Home';
+
+export default function Home() {
+  return <>
+    <h1>{title}</h1>
+    <p>Hello World!</p>
+  </>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+import { Metadata } from 'next';
+import Home, { title } from './Home';
+
+export const metadata: Metadata = {title};
+
+export default function HomePage() {
+  return <Home/>;
 }
