97fbda0d26e747b94976edeca73dd37836121205
Étienne Miret <etienne.miret@ens-lyon.org>
2023-06-08 18:27:41 +0200

    Use utility function in server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { Metadata } from 'next';
 import Home, { title } from './Home';
+import { fullTitle } from '../utils';
 
-export const metadata: Metadata = {title};
+export const metadata: Metadata = {
+  title: fullTitle(title),
+};
 
 export default function HomePage() {
   return <Home/>;
---
+++ utils.ts
@@ -0,0 +1,3 @@
+export function fullTitle(title: string): string {
+  return `Next.js MCVE - ${title}`;
+}
